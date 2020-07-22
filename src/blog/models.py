53cd86e6d9988a8c2251677d53d90acde6dd4b6e
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-07-22 22:14:58 +0200

    No. 39 Associate Blog Post to a User - Foreign Key

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,11 +1,13 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
 
-class BlogPost(models.Model):   # after creating new model, make sure it is in the apps (settings.py) and type in terminal "python manage.py makemigrations" and "python manage.py migrate"
+
+User = settings.AUTH_USER_MODEL
+class BlogPost(models.Model):   # blogpost_set->queryset; after creating new model, make sure it is in the apps (settings.py) and type in terminal "python manage.py makemigrations" and "python manage.py migrate"
     # id = models.IntegerField()    # primary key (pk)
+    user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)       # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
-
-# no. 19 Model to Django Admin

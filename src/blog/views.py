8e473ab30eae080abfcbec3cba92cbe1f373d26d
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-07-25 20:20:17 +0200

    No. 43 Delete and Confirm

--- src/blog/views.py
+++ src/blog/views.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.decorators import login_required
 from django.contrib.admin.views.decorators import staff_member_required
 from django.http import Http404
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 
 # Create your views here.
 # no. 20 Model in a View
@@ -52,6 +52,7 @@ def blog_post_detail_view(request, slug):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_update_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     form = BlogPostModelForm(request.POST or None, instance=obj)
@@ -62,8 +63,12 @@ def blog_post_update_view(request, slug):
     return render(request, template_name, context)
 
 
+@staff_member_required
 def blog_post_delete_view(request, slug):
     obj = get_object_or_404(BlogPost, slug=slug)
     template_name = 'blog/delete.html'
+    if request.method == "POST":
+        obj.delete()
+        return redirect("/blog")
     context = {"object": obj}
     return render(request, template_name, context)

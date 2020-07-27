189acb97c9a5f94f275fc802a4c55dab73034cb4
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-07-27 22:48:17 +0200

    No. 46 Include with Arguments

--- src/try_django/views.py
+++ src/try_django/views.py
@@ -7,11 +7,13 @@ from django.template.loader import get_template
 # Don't Repeat Yourself = DRY
 
 from .forms import ContactForm
+from blog.models import BlogPost
+
+
 def home_page(request):
     my_title = "Hello there..."
-    context = {"title": "my_title"}
-    if request.user.is_authenticated:
-        context = {"title": my_title, "my_list": [1, 2, 3, 4]}
+    qs = BlogPost.objects.all()[:5]
+    context = {"title": "Welcome to Try Django", "blog_list": qs}
     return render(request, "home.html", context)
 
 

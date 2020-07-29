8fcfc8ebf879e2e0dbb83bda78e247745b84aebd
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-07-29 17:27:28 +0200

    No. 48 Publish Date, Timestamp and Updated

--- src/blog/models.py
+++ src/blog/models.py
@@ -11,6 +11,12 @@ class BlogPost(models.Model):   # blogpost_set->queryset; after creating new mod
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)       # hello world -> hello-world
     content = models.TextField(null=True, blank=True)
+    publish_date = models.DateTimeField(auto_now=False, auto_now_add=False, null=True, blank=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-publish_date', '-updated', '-timestamp']
 
     def get_absolute_url(self):
         return f"/blog/{self.slug}"

c35e1c9ba76dccdf15cc374c77e5de5dcedf3dd7
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-08-21 22:21:36 +0200

    No. 51 Static Files and Uploading Files

--- src/blog/models.py
+++ src/blog/models.py
@@ -22,6 +22,7 @@ class BlogPostManager(models.Manager):
 class BlogPost(models.Model):   # blogpost_set->queryset; after creating new model, make sure it is in the apps (settings.py) and type in terminal "python manage.py makemigrations" and "python manage.py migrate"
     # id = models.IntegerField()    # primary key (pk)
     user = models.ForeignKey(User, default=1, null=True, on_delete=models.SET_NULL)
+    image = models.FileField(upload_to='image/', blank=True, null=True)
     title = models.CharField(max_length=120)
     slug = models.SlugField(unique=True)       # hello world -> hello-world
     content = models.TextField(null=True, blank=True)

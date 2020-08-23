9ea50d0f6509300b26ecaf4f327cc76a3d8b63bb
Bartosz Rudziński <56740439+Barud21@users.noreply.github.com>
2020-08-23 13:04:25 +0200

    No 54 Complex Lookups

--- src/blog/models.py
+++ src/blog/models.py
@@ -1,5 +1,6 @@
 from django.conf import settings
 from django.db import models
+from django.db.models import Q
 from django.utils import timezone
 # Create your models here.
 
@@ -11,6 +12,18 @@ class BlogPostQuerySet(models.QuerySet):
         now = timezone.now()
         # BlogPost.objects.all()
         return self.filter(publish_date__lte=now)
+    
+    def search(self, query):
+        lookup = (
+                    Q(title__icontains=query) | 
+                    Q(content__icontains=query) |
+                    Q(slug__icontains=query) |
+                    Q(user__first_name__icontains=query) |
+                    Q(user__last_name__icontains=query) |
+                    Q(user__username__icontains=query)
+        )
+        
+        return self.filter(lookup)
 
 class BlogPostManager(models.Manager):
     def get_queryset(self):
@@ -18,6 +31,11 @@ class BlogPostManager(models.Manager):
 
     def published(self):
         return self.get_queryset().published()
+
+    def search(self, query=None):
+        if query is None:
+            return self.get_queryset().none()
+        return self.get_queryset().published().search(query)
 
 class BlogPost(models.Model):   # blogpost_set->queryset; after creating new model, make sure it is in the apps (settings.py) and type in terminal "python manage.py makemigrations" and "python manage.py migrate"
     # id = models.IntegerField()    # primary key (pk)
